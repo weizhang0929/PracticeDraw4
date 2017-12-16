4748d12bb9a5b9b3e18f497ffe0e528265a725b7
zhangwei0929 <zhangwei2014_mail@163.com>
2017-12-16 21:05:45 +0800

    练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -17,6 +18,7 @@ public class Practice12CameraRotateFixedView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Camera camera = new Camera();
 
     public Practice12CameraRotateFixedView(Context context) {
         super(context);
@@ -38,7 +40,31 @@ public class Practice12CameraRotateFixedView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+        int center1X = point1.x + bitmapWidth / 2;
+        int center1Y = point1.y + bitmapHeight / 2;
+        int center2X = point2.x + bitmapWidth / 2;
+        int center2Y = point2.y + bitmapHeight / 2;
+
+        canvas.save();
+        camera.save();
+        camera.rotateX(30);
+        canvas.translate(center1X, center1Y);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-center1X, -center1Y);
+        camera.restore();
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        camera.save();
+        camera.rotateY(30);
+        canvas.translate(center2X, center2Y);
+        camera.applyToCanvas(canvas);
+        canvas.translate(-center2X, -center2Y);
+        camera.restore();
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice14FlipboardView.java
@@ -73,7 +73,16 @@ public class Practice14FlipboardView extends View {
         int y = centerY - bitmapHeight / 2;
 
         canvas.save();
+        canvas.clipRect(0, 0, getWidth(), centerY);
+        canvas.drawBitmap(bitmap, x, y, paint);
+        canvas.restore();
 
+        canvas.save();
+        if (degree > 90) {
+            canvas.clipRect(0, 0, getWidth(), centerY);
+        } else {
+            canvas.clipRect(0, centerY, getWidth(), getHeight());
+        }
         camera.save();
         camera.rotateX(degree);
         canvas.translate(centerX, centerY);
